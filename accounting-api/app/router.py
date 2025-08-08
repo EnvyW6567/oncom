2111ed33cbe2c46f5960a385b7102b6d80741d3b
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-08-08 23:31:20 +0900

    feat: process 엔드포인트 구현

--- accounting-api/app/router.py
+++ accounting-api/app/router.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, Depends, File
+
+from .service.accounting_service import AccountingService
 
 router = APIRouter(prefix="/api/v1/accounting")
 
 
 @router.post("/process")
-async def process_transactions():
-    pass
+async def process_transactions(
+        transactions_file: UploadFile = File,
+        rules_file: UploadFile = File,
+        accounting_service: AccountingService = Depends(AccountingService)
+):
+    job = await accounting_service.process(transactions_file, rules_file)
+
+    return job
 
 
 @router.get("/records")

288f8686eeeff506eb1bd35782ac0ebd48e33d1c
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-08-08 14:30:29 +0900

    chore: accounting-api 의존성
    - requirements.txt

---
+++ accounting-api/app/database.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+USER = os.getenv("POSTGRES_USER")
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB = os.getenv("POSTGRES_DATABASE")
+PORT = os.getenv("POSTGRES_PORT", "5432")
+HOST = os.getenv("POSTGRES_HOST", "postgres")
+
+DATABASE_URL = f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST}:{PORT}/${DB}"
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session

f9173a113fb2e9ce0422c8c9bc5a5c08bdb5c3bc
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-08-09 11:36:31 +0900

    feat: company_records 쿼리 구현

--- accounting-api/app/repository/impl/postgres_processing_job_repository.py
+++ accounting-api/app/repository/impl/postgres_processing_job_repository.py
@@ -1,12 +1,17 @@
+from typing import List
+
 from fastapi import Depends
+from sqlalchemy import and_, desc, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..processing_job_repository import ProcessingJobRepository
 from ...database.postgres_db import get_postgres
-from ...model import ProcessingJob
+from ...model import ProcessingJob, Transaction, Category
+from ...schema import CompanyRecord
 
 
 class PostgresProcessingJobRepository(ProcessingJobRepository):
+
     def __init__(self, session: AsyncSession = Depends(get_postgres)):
         self.__session = session
 
@@ -17,3 +22,23 @@ class PostgresProcessingJobRepository(ProcessingJobRepository):
         except Exception as e:
             await self.__session.rollback()
             raise e
+
+    async def find_company_records(self, company_id: int) -> List[CompanyRecord]:
+        query = select(
+            Transaction.transaction_id,
+            Transaction.transaction_date,
+            Transaction.category_id,
+            Transaction.transaction_date,
+            Category.category_name
+        ).outerjoin(
+            Category, Transaction.category_id == Category.category_id
+        ).where(
+            and_(
+                Transaction.company_id == company_id,
+                Transaction.deleted_at is None
+            )
+        ).order_by(desc(Transaction.transaction_date))
+
+        result = await self.__session.execute(query)
+
+        return list(map(CompanyRecord.model_validate, result.mappings()))
--- accounting-api/app/repository/processing_job_repository.py
+++ accounting-api/app/repository/processing_job_repository.py
@@ -1,9 +1,15 @@
 from abc import ABCMeta, abstractmethod
+from typing import List
 
 from ..model import ProcessingJob
+from ..schema import CompanyRecord
 
 
 class ProcessingJobRepository(metaclass=ABCMeta):
     @abstractmethod
     async def save(self, processing_job: ProcessingJob):
+        pass
+
+    @abstractmethod
+    async def find_company_records(self, company_id: int) -> List[CompanyRecord]:
         pass
